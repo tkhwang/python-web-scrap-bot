9a4ef73196f7d222f2f54621b94c59b067c419d8
tkhwang <tkhwang@gmail.com>
2017-12-04 11:25:02 +0900

    [+] Slack configuration added.

--- deploy/slackbot.py
+++ deploy/slackbot.py
@@ -1,11 +1,13 @@
 from slacker import Slacker
 
+SLACK_TOKEN = ''
+SLACK_BOARD = ''
 
 class SlackBot:
     def __init__(self):
-        self.token = ''
+        self.token = SLACK_TOKEN
         self.slack = Slacker(self.token)
-        self.slack_board = '#z-bot'
+        self.slack_board = SLACK_BOARD
 
     def send_message(self, msg):
         self.slack.chat.post_message(self.slack_board, msg)
